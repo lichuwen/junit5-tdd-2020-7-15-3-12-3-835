1a05ac1d71202d3b525f3805d45c40be7789d95f
lichuwen <545759585@qq.com>
2020-07-23 11:47:18 +0800

    [karen] modify:modify play function

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -2,6 +2,13 @@ package junit5.tdd;
 
 public class FizzBuzzWhizz {
     public String play(int number) {
-        return String.valueOf(number);
+        String result = "";
+        if(number % 3 == 0){
+            result = "Fizz";
+        }
+        else{
+            result = String.valueOf(number);
+        }
+        return result;
     }
 }
